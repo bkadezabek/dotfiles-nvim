cdca4c206ace8afca104e4096ee4417a0f8adc67
bkadezabek <borna.kadezabek3@gmail.com>
2025-02-14 23:42:18 +0100

    Added mini.files
    Leader E

--- init.lua
+++ init.lua
@@ -294,6 +294,7 @@ require('lazy').setup({
   require 'custom.plugins.nvim-tree',
   require 'custom.plugins.noice-nvim',
   require 'custom.plugins.nvim-notify',
+  require 'custom.plugins.mini-nvim',
   -- NOTE: END OF CUSTOM PLUGINS
 
   { -- Useful plugin to show you pending keybinds.
@@ -697,6 +698,18 @@ require('lazy').setup({
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
+            if server_name == 'pylsp' then
+              server.settings = {
+                pylsp = {
+                  plugins = {
+                    pycodestyle = {
+                      ignore = { 'E501' }, -- Disable line-too-long warning
+                      maxLineLength = 100, -- Optional: Set a custom max line length
+                    },
+                  },
+                },
+              }
+            end
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
@@ -870,45 +883,6 @@ require('lazy').setup({
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
-    end,
-  },
-
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
