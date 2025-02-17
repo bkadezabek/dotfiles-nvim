5437a92009556feff4310164ed0271afa724d420
bkadezabek <borna.kadezabek3@gmail.com>
2025-02-17 12:33:21 +0100

    Fixed labels to be more intuitive in flash. Also made the flash toggle be always on on startup

--- lua/custom/plugins/flash-nvim.lua
+++ lua/custom/plugins/flash-nvim.lua
@@ -1,12 +1,23 @@
 return {
   'folke/flash.nvim',
   event = 'VeryLazy',
-  opts = {},
+  opts = {
+    labels = 'qwertasdfgzxcvbyuiophjklnm',
+    label = {
+      reuse = 'none', -- Assign labels in sequential order without reuse
+    },
+  },
+  config = function(_, opts)
+    require('flash').setup(opts)
+    -- Schedule the toggle to run after initialization
+    vim.schedule(function()
+      require('flash').toggle()
+    end)
+  end,
   -- stylua: ignore
   keys = {
     { "<c-/>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "<leader>sv", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    -- { "<leader>sž", mode = {"n", "x", "o"}, function() require("flash").remote() end, desc = "Remote Flash" },
     { "<leader>sV", mode = {"n", "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     -- Toggle Flash Search is within the command mode indicated by "c" mode. 
     -- Shows flash within the search / with the jump labels. needs to be toggled!
